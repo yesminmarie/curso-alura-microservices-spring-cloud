0ab60ef4bed123d199f34d320a7558ebb10c3356
Yesmin Lahoud <ymslahoud@gmail.com>
2021-08-11 20:22:42 -0300

    Configurando o Papertrail

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.loja.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -17,13 +19,19 @@ import br.com.alura.microservice.loja.model.Compra;
 @Service
 public class CompraService {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
+	
 	@Autowired
 	private FornecedorClient fornecedorClient;
 
 	public Compra realizaCompra(CompraDTO compra) {
 		
+		final String estado = compra.getEndereco().getEstado();
+		
+		LOG.info("buscando informações do fornecedor de {}", estado);
 		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
 		
+		LOG.info("realizando um pedido");
 		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
 	
 		System.out.println(info.getEndereco());
